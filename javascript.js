e3eedeb94cb49555d8788a88ea25a941431b21f6
MaciejStrozniak <maciejstrozniak@gmail.com>
2024-12-10 13:06:21 +0100

    Update to calculator object in javascript.js. Now it creates full calculator in index.html.

--- javascript.js
+++ javascript.js
@@ -4,33 +4,53 @@ const bodyEl = document.querySelector('body');
 function Calculator(node) {
     this.calculator = document.createElement('div');
     this.inputField = document.createElement('input');
-    this.resetBtn = document.createElement('button');
+    this.reset = document.createElement('button');
     this.digitContainer = document.createElement('div');
     this.operationsContainer = document.createElement('div');
     this.sum = document.createElement('button');
 
     this.buildCalc = function() {
 
-        for(let i = 0; i < 9; i++) {
+        for(let i = 0; i < 10; i++) {
             this.digit = document.createElement('button');
+            this.digit.textContent = `${i}`;
             this.digitContainer.appendChild(this.digit); 
         };
 
         for(let i = 0; i < 4; i++) {
-            this.operations = document.createElement('button');
-            this.operationsContainer.appendChild(this.operations);
+            this.operation = document.createElement('button');
+
+            switch (i) {
+                case 0:
+                    this.operation.textContent = '/';
+                    break;
+                case 1:
+                    this.operation.textContent = '*';
+                    break;
+                case 2:
+                    this.operation.textContent = '-';
+                    break;
+                case 3:
+                    this.operation.textContent = '+';
+                    break;
+            }
+
+            this.operationsContainer.appendChild(this.operation);
         };
 
+        this.reset.textContent = 'AC';
+        this.sum.textContent = '='
+
         this.calculator.appendChild(this.inputField);
-        this.calculator.appendChild(this.resetBtn);
+        this.calculator.appendChild(this.reset);
         this.calculator.appendChild(this.digitContainer);
         this.calculator.appendChild(this.operationsContainer);
-      
-
         this.calculator.appendChild(this.sum);
 
         node.appendChild(this.calculator);
     };
+
+
 };
 
 const calc1 = new Calculator(bodyEl);
